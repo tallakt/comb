7fa259a4ed2abaf0cc53d0b77a313f220ccb170e
alan blount <alan@zeroasterisk.com>
2018-01-06 17:35:06 -0700

    Update mix.exs
    warning: variable "deps" does not exist and is being expanded to "deps()", please use parentheses to remove the ambiguity or change the variable name

--- mix.exs
+++ mix.exs
@@ -3,11 +3,11 @@ defmodule Comb.Mixfile do
 
   def project do
     [app: :comb,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps()]
   end
 
   # Configuration for the OTP application
